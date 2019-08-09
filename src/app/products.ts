72d76c411e6a06eab7ef0699b9a765f52fd3eb99
Fadil Karajic <fadil.karajic@gmail.com>
2019-08-08 21:34:08 -0500

    Angular 8 project (Store)
    Started Angular 8 project from scratch.

---
+++ src/app/products.ts
@@ -0,0 +1,22 @@
+export const products = [
+    {
+      name: 'Orange',
+      price: 1.00,
+      description: 'Fruit'
+    },
+    {
+      name: 'Carrot',
+      price: 0.99,
+      description: 'Vegetable'
+    },
+    {
+      name: 'Sirloin steak',
+      price: 11.0,
+      description: 'Meat'
+    },
+    {
+      name: 'Test product',
+      price: 11.0,
+      description: ''
+    }
+  ];
